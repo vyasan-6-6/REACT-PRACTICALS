3c7790a7e325e64807e3a40b26262346ca3374fd
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-02-21 14:13:03 +0530

    Clean global implementation by css variable

--- src/Redux-components/Theme.jsx
+++ src/Redux-components/Theme.jsx
@@ -6,14 +6,20 @@ const Theme = () => {
     const mode = useSelector((state)=>state.theme.mode)
     const dispatch =useDispatch();
 
+
+    useEffect(() => {
+      document.documentElement.setAttribute("Data-theme",mode)
+     
+    }, [mode])
+    
     
   return (
     <div style={{
        
-        width:"100vw",
-        height:"100vh",
-        color:mode === 'light' ? "black" :mode==="dark" ?"#e0e1dd":"red",
-        backgroundColor:mode==="light"?"white": mode ==="dark" ? "#0d1b2a" : "royalblue",
+        // width:"100vw",
+        // height:"100vh",
+        // color:mode === 'light' ? "black" :mode==="dark" ?"#e0e1dd":"red",
+        // backgroundColor:mode==="light"?"white": mode ==="dark" ? "#0d1b2a" : "royalblue",
     }}> <div className=" mx-auto text-center">
 
         <h1>{mode.toUpperCase() }mode</h1>
